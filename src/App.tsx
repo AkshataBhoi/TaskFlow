0de31053eec884c8488ec6096bbd4a08e3537931
Akshata Bhoi <akshatabbhoi@gmail.com>
2026-08-08 22:45:17 +0530

    /login is starting point

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 import Login from './pages/Login';
 import DashboardLayout from './layouts/DashboardLayout';
 import Dashboard from './pages/Dashboard';
@@ -6,6 +7,7 @@ import Tasks from './pages/Tasks';
 import Categories from './pages/Categories';
 import History from './pages/History';
 import Settings from './pages/Settings';
+
 import { AuthProvider } from './context/AuthContext';
 import { Toaster } from 'react-hot-toast';
 import ProtectedRoute from './components/ProtectedRoute';
@@ -15,22 +17,36 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
+
+          {/* Starting point */}
+          <Route path="/" element={<Navigate to="/login" replace />} />
+
+          {/* Login */}
           <Route path="/login" element={<Login />} />
-          
+
+          {/* Protected Application */}
           <Route element={<ProtectedRoute />}>
-            <Route path="/" element={<DashboardLayout />}>
-              <Route index element={<Navigate to="/dashboard" replace />} />
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="my-tasks" element={<Tasks />} />
-              <Route path="categories" element={<Categories />} />
-              <Route path="history" element={<History />} />
-              <Route path="settings" element={<Settings />} />
+            <Route element={<DashboardLayout />}>
+
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/my-tasks" element={<Tasks />} />
+              <Route path="/categories" element={<Categories />} />
+              <Route path="/history" element={<History />} />
+              <Route path="/settings" element={<Settings />} />
+
             </Route>
           </Route>
+
+          {/* Any unknown URL */}
+          <Route path="*" element={<Navigate to="/login" replace />} />
+
         </Routes>
+
+        <Toaster position="top-right" />
+
       </BrowserRouter>
-      <Toaster position="top-right" />
     </AuthProvider>
-)}
+  );
+}
 
 export default App;
